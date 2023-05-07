b621249fa5988d543b45e484818fd036b52fae5a
Zatey <michael.zatey@kibo.school>
2023-05-07 11:15:29 -0700

    New update to courasel

--- script.js
+++ script.js
@@ -46,3 +46,69 @@ $(document).ready(function() {
         developmentIsVisible = true;
       }
     }); // -- End window scroll --
+const options = {
+  cutoutPercentage: 65,
+  animation: {
+    easing: 'easeOutQuint'
+  },
+  plugins: {
+    tooltip: {
+      enabled: false
+    }
+  },
+  events: []
+};
+
+const skills = [
+  {
+    id: "html_css",
+    values: [95, 5]
+  },
+  {
+    id: "sass",
+    values: [90, 10]
+  },
+  {
+    id: "jquery",
+    values: [90, 10]
+  },
+  {
+    id: "rails",
+    values: [66, 34]
+  },
+  {
+    id: "backbone",
+    values: [75, 25]
+  },
+  {
+    id: "photoshop",
+    values: [66, 34]
+  }
+];
+
+let offset = 0;
+
+for (const skill of skills) {
+  const canvas = document.querySelector(`#${skill.id}`);
+  if (!canvas) { continue; }
+
+  const ctx = canvas.getContext('2d');
+  
+  setTimeout(() => {
+    const chart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          data: skill.values,
+          backgroundColor: [
+            'hsl(120, 40%, 60%)',
+            'hsl(0, 0%, 95%)',
+          ]
+        }]
+      },
+      options: options
+    });
+  }, offset);
+
+  offset += 250;
+}
